2b9649ab6103e1971f9089d410cd4dda93f11c82
Mr.GeeKy <mr.geeky1992@gmail.com>
2024-05-23 11:07:29 +0430

    export & import modules

--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CollectionsRoutingModule } from './collections-routing.module';
+import { CollectionHomeComponent } from './collection-home/collection-home.component';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    CollectionsRoutingModule
-  ]
+    CollectionsRoutingModule,CollectionHomeComponent
+  ],
+  exports:[CollectionHomeComponent]
 })
 export class CollectionsModule { }
